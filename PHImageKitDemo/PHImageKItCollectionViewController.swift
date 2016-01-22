49ba379bf0e3b3ca76a966cb4f9f4f786470d5e8
DeVladinci <v.vladimirov@icloud.com>
2016-01-22 16:15:57 +0200

    Initial demo project

---
+++ PHImageKitDemo/PHImageKItCollectionViewController.swift
@@ -0,0 +1,61 @@
+//
+//  PHImageKItCollectionViewController.swift
+//  PHImageKit
+//
+//  Created by Vlado on 1/22/16.
+//  Copyright © 2016 ProductHunt. All rights reserved.
+//
+
+import UIKit
+import PHImageKit
+
+private let reuseIdentifier = "PHImageKitCellIdentifier"
+
+class PHImageKItCollectionViewController: UICollectionViewController, PHImageKitDataSourceDelegate {
+
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+
+    private let imageDataSource = PHImageKitDataSource()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imageDataSource.loadData(withType: DataType.GIF)
+        imageDataSource.delegate = self
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageDataSource.content.count
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! PHImageKitCell
+    
+        cell.imageView.url = imageDataSource.content[indexPath.row]
+
+        return cell
+    }
+
+    // MARK: Actions
+
+    @IBAction func changeSelectedSegmentAction(sender: UISegmentedControl) {
+        imageDataSource.loadData(withType: sender.selectedSegmentIndex == 0 ? DataType.GIF : DataType.Image)
+    }
+
+    @IBAction func reload(sender: AnyObject) {
+        contentChanged()
+    }
+
+    @IBAction func clearCache(sender: AnyObject) {
+        PHManager.sharedInstance.purgeCache(includingFileCache: true)
+    }
+
+    // MARK: PHImageKitDataSourceDelegate
+
+    func contentChanged() {
+        collectionView?.reloadData()
+    }
+
+}
---
+++ PHImageKitDemo/PHImageKitCell.swift
@@ -0,0 +1,16 @@
+//
+//  PHImageKitCell.swift
+//  PHImageKit
+//
+//  Created by Vlado on 1/22/16.
+//  Copyright © 2016 ProductHunt. All rights reserved.
+//
+
+import UIKit
+import PHImageKit
+
+class PHImageKitCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: PHImageView!
+
+}
