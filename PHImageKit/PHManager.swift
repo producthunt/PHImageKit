5eb8692888351596ca7300b8227bc319ccd9f336
DeVladinci <v.vladimirov@icloud.com>
2016-01-22 12:31:20 +0200

    Documentation for PHManager

--- PHImageKit/PHManager.swift
+++ PHImageKit/PHManager.swift
@@ -16,12 +16,24 @@ typealias PHManagerCompletion = (object: PHImageObject?) -> Void
 
 class PHManager: NSObject {
 
+    /// Shared Instance
     static let sharedInstance = PHManager()
 
     private let downloader = PHDownloader()
     private let cache = PHCache()
 
     //TODO: (Vlado) Handle local url cases.
+
+    /**
+        Download image from given url
+    
+        - parameters:
+            - url: Image URL
+            - progress: Progress closure returns downloaded percent
+            - completion: Completion closure returns `PHImageObject` that holds image or gif.
+        
+        - returns: Unique download key
+    */
     func imageWithUrl(url: NSURL, progress: PHProgressCompletion, completion: PHManagerCompletion) -> String? {
         if imageFromCache(url, completion: completion) {
             return nil;
@@ -30,12 +42,26 @@ class PHManager: NSObject {
         return imageFromWeb(url, progress: progress, completion: completion)
     }
 
+    /**
+     Get image from given url
+     
+     - parameters:
+        - url: Image URL
+        - completion: Completion closure returns image
+     */
     func imageWithUrl(url: NSURL, completion: ((image: UIImage?) -> Void)) {
         imageWithUrl(url, progress: { (percent) -> Void in }) { (object) -> Void in
             completion(image: object?.image)
         }
     }
 
+    /**
+     Get image data from given url
+
+     - parameters:
+        - url: Image URL
+        - completion: Completion closure returns raw image data
+    */
     func imageData(url: NSURL, comletion: ((data: NSData) -> Void)) {
         imageWithUrl(url, progress: { (percent) -> Void in }) { (object) -> Void in
             if let object = object, data = object.data {
@@ -44,10 +70,24 @@ class PHManager: NSObject {
         }
     }
 
+    /**
+    Cancel retrieve of ongoing image download request
+     
+    - parameters:
+        - url: Image URL
+        - key: Unique key returned from `imageWithUrl:`
+
+    */
     func cancelImageRetrieve(url: NSURL, key: String) {
         downloader.cancel(url, key: key)
     }
 
+    /**
+     Purge memory and file cache
+     
+     - parameters: 
+        - includingFileCache: If `true` file cache will be purged.
+    */
     func purgeCache(includingFileCache fileCache: Bool, completion: PHVoidCompletion? = nil) {
         cache.clearMemoryCache()
 
